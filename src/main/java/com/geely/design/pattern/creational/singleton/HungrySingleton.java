8484efff74aca2ed93b2a20f71e171e5ba2d60a6
Li-Rhine <39134469+Li-Rhine@users.noreply.github.com>
2019-10-23 01:43:10 +0800

    设计模式-单例模式(序列化破环单例模式以及其解决方法)
    序列化内部使用反射，反射会创建多个对象，所以增加readResolve()方法，返回初始化的对象。序列化内部会根据反射先判断是否有readResolve()方法，如果没有就重新初始化一个对象，如果有就直接调用readResolve()

--- src/main/java/com/geely/design/pattern/creational/singleton/HungrySingleton.java
+++ src/main/java/com/geely/design/pattern/creational/singleton/HungrySingleton.java
@@ -1,6 +1,8 @@
 package com.geely.design.pattern.creational.singleton;
 
-public class HungrySingleton {
+import java.io.Serializable;
+
+public class HungrySingleton implements Serializable {
     private final static HungrySingleton hungrySingleton;
 
     static {
@@ -10,6 +12,11 @@ public class HungrySingleton {
     private HungrySingleton(){};
 
     public static HungrySingleton getInstance(){
+        return hungrySingleton;
+    }
+
+    //防止序列化破坏单例模式（内部原因是因为序列化内部采用了反射获取对象，所以会出现不是同一个对象的情况）
+    private Object readResolve(){
         return hungrySingleton;
     }
 }
--- src/main/java/com/geely/design/pattern/creational/singleton/Test.java
+++ src/main/java/com/geely/design/pattern/creational/singleton/Test.java
@@ -1,12 +1,26 @@
 package com.geely.design.pattern.creational.singleton;
 
+import java.io.*;
+
 public class Test {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
 //        LazySingleton lazySingleton = LazySingleton.getInstance();
-        Thread t1 = new Thread(new T());
-        Thread t2 = new Thread(new T());
-        t1.start();
-        t2.start();
-        System.out.println("progam end");
+//        Thread t1 = new Thread(new T());
+//        Thread t2 = new Thread(new T());
+//        t1.start();
+//        t2.start();
+//        System.out.println("progam end");
+
+        HungrySingleton instance = HungrySingleton.getInstance();
+        ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("singleton_file"));
+        oos.writeObject(instance);
+
+        File file = new File("singleton_file");
+        ObjectInputStream ois = new ObjectInputStream(new FileInputStream(file));
+        HungrySingleton newInstance = (HungrySingleton) ois.readObject();
+
+        System.out.println(instance);
+        System.out.println(newInstance);
+        System.out.println(instance == newInstance);
     }
 }
