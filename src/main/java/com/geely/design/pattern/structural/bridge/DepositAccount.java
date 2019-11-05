f3346821c6a0f0fc341eb2a942a018c754b09a59
Li-Rhine <39134469+Li-Rhine@users.noreply.github.com>
2019-11-06 01:59:36 +0800

    设计模式-桥接模式

---
+++ src/main/java/com/geely/design/pattern/structural/bridge/DepositAccount.java
@@ -0,0 +1,13 @@
+package com.geely.design.pattern.structural.bridge;
+
+public class DepositAccount implements Account {
+    public Account openAccount() {
+        System.out.println("打开定期账号");
+        return new DepositAccount();
+    }
+
+    public void showAccountType() {
+        System.out.println("这是一个定期账号");
+
+    }
+}
