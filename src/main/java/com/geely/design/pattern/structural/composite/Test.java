0c75c32153294b42bc948260f14a697b582477b6
Li-Rhine <39134469+Li-Rhine@users.noreply.github.com>
2019-11-06 00:18:25 +0800

    设计模式-组合模式

---
+++ src/main/java/com/geely/design/pattern/structural/composite/Test.java
@@ -0,0 +1,25 @@
+package com.geely.design.pattern.structural.composite;
+
+public class Test {
+    public static void main(String[] args) {
+        CatalogComponent linuxCourse = new Course("Linux课程",11);
+        CatalogComponent windowsCourse = new Course("Windows课程",11);
+
+        CatalogComponent javaCourseCatalog = new CourseCatalog("java课程目录",2);
+
+        CatalogComponent mmallCourse1 = new Course("java电商一期",55);
+        CatalogComponent mmallCourse2 = new Course("java电商二期",66);
+        CatalogComponent designPattern = new Course("java设计模式",77);
+
+        javaCourseCatalog.add(mmallCourse1);
+        javaCourseCatalog.add(mmallCourse2);
+        javaCourseCatalog.add(designPattern);
+
+        CatalogComponent imoocMainCourseCatalog = new CourseCatalog("慕课网课程主目录",1);
+        imoocMainCourseCatalog.add(linuxCourse);
+        imoocMainCourseCatalog.add(windowsCourse);
+        imoocMainCourseCatalog.add(javaCourseCatalog);
+
+        imoocMainCourseCatalog.print();
+    }
+}
