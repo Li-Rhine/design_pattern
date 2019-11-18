1a9722be449232de93b2ada506e4be4164ac7a5e
Li-Rhine <39134469+Li-Rhine@users.noreply.github.com>
2019-11-19 02:22:01 +0800

    设计模式-命令模式

--- src/main/java/com/geely/design/pattern/behavioral/command/OpenCourseVideoCommand.java
+++ src/main/java/com/geely/design/pattern/behavioral/command/OpenCourseVideoCommand.java
@@ -1,6 +1,5 @@
 package com.geely.design.pattern.behavioral.command;
 
-import com.geely.design.pattern.behavioral.observer.Course;
 
 public class OpenCourseVideoCommand implements Command {
     private CourseVideo courseVideo;
--- src/main/java/com/geely/design/pattern/behavioral/command/Test.java
+++ src/main/java/com/geely/design/pattern/behavioral/command/Test.java
@@ -1,6 +1,5 @@
 package com.geely.design.pattern.behavioral.command;
 
-import java.util.Stack;
 
 public class Test {
     public static void main(String[] args) {
